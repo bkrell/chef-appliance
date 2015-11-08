0ad9ac52f26d2483fe7e09e6a35d3119143e4d55
bkrell <bkrell@gmail.com>
2015-11-08 08:42:30 -0800

    configure recipe do dl and copy the source zip

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,20 @@ include_recipe 'apt::default'
 package 'apache2'
 package 'mysql-server'
 package 'unzip'
+
+# download and copy source zip
+script 'download_and_copy' do
+  interpreter "bash"
+  cwd '/tmp/'
+  code <<-EOH
+    wget https://github.com/colincam/Awesome-Appliance-Repair/archive/master.zip
+    unzip master.zip
+    cd Awesome-Appliance-Repair-master
+    sudo mv AAR /var/www/
+    EOH
+  not_if { ::File.exists?'/var/www/AAR/awesomeapp.py' }
+end
+
+# can't just use execute here because it is not idempotent
+# need guards, e.g. not_if, also try script block instead of execute
+#execute 'wget https://github.com/colincam/Awesome-Appliance-Repair/archive/master.zip'
