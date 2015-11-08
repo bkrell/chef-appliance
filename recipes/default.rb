2b2904d6f0f5d07d11b435d9f6b43cd1910dbc72
bkrell <bkrell@gmail.com>
2015-11-08 10:30:26 -0800

    update dl code, add code to install app

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,6 +19,17 @@ script 'download_and_copy' do
   not_if { ::File.exists?'/var/www/AAR/awesomeapp.py' }
 end
 
-# can't just use execute here because it is not idempotent
-# need guards, e.g. not_if, also try script block instead of execute
-#execute 'wget https://github.com/colincam/Awesome-Appliance-Repair/archive/master.zip'
+# problem with install script is getpass prompt so replace it
+execute "sudo sed -ibak s/getpass\\\\./\\'\\'#/g /tmp/Awesome-Appliance-Repair-master/AARinstall.py"
+
+# now run the python script, unless already installed (look for config file)
+execute 'install AAR app' do
+  cwd '/tmp/Awesome-Appliance-Repair-master/'
+  command 'sudo python AARinstall.py'
+  not_if { ::File.exists?'/etc/apache2/sites-enabled/AAR-apache.conf' }
+end
+
+# restart apache for changes to take effect
+service 'apache2' do
+  action :restart
+end
